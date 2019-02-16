33551a9b5c2db95a003b26806ef4834cbcd38470
Marvin Zhang <tikazyq@gmail.com>
2019-02-16 22:12:34 +0800

    added CRUD for NodeList

--- src/store/modules/node.js
+++ src/store/modules/node.js
@@ -11,16 +11,35 @@ const getters = {}
 const mutations = {
   SET_NODE_FORM (state, value) {
     state.nodeForm = value
+  },
+  SET_NODE_LIST (state, value) {
+    state.nodeList = value
   }
 }
 
 const actions = {
-  addNode ({ state }) {
+  getNodeList ({ state, commit }) {
+    request.get('/nodes', {})
+      .then(response => {
+        commit('SET_NODE_LIST', response.data.items)
+      })
+  },
+  addNode ({ state, dispatch }) {
     request.put('/nodes', {
-      node_name: state.nodeForm.name,
-      node_ip: state.nodeForm.ip,
-      node_description: state.nodeForm.description
+      name: state.nodeForm.name,
+      ip: state.nodeForm.ip,
+      port: state.nodeForm.port,
+      description: state.nodeForm.description
     })
+      .then(() => {
+        dispatch('getNodeList')
+      })
+  },
+  deleteNode ({ state, dispatch }, id) {
+    request.delete(`/nodes/${id}`)
+      .then(() => {
+        dispatch('getNodeList')
+      })
   }
 }
 
--- src/views/node/NodeList.vue
+++ src/views/node/NodeList.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <!--add popup-->
     <el-dialog
-      title="Add Node"
+      :title="isEditMode ? 'Edit Node' : 'Add Node'"
       :visible.sync="dialogVisible"
       width="60%"
       :before-close="onDialogClose">
@@ -17,8 +17,8 @@
         <el-form-item label="IP">
           <el-input v-model="nodeForm.ip" placeholder="IP"></el-input>
         </el-form-item>
-        <el-form-item label="PORT">
-          <el-input v-model="nodeForm.port" type="number" placeholder="PORT"></el-input>
+        <el-form-item label="Port">
+          <el-input v-model="nodeForm.port" type="number" placeholder="Port"></el-input>
         </el-form-item>
         <el-form-item label="Description">
           <el-input v-model="nodeForm.description" placeholder="Description" type="textarea"></el-input>
@@ -61,7 +61,8 @@
                          :width="col.width">
           <template slot-scope="scope">
             <el-tag type="info" v-if="scope.row.status === 0">Offline</el-tag>
-            <el-tag type="success" v-if="scope.row.status === 1">Online</el-tag>
+            <el-tag type="success" v-else-if="scope.row.status === 1">Online</el-tag>
+            <el-tag type="danger" v-else>Unavailable</el-tag>
           </template>
         </el-table-column>
         <el-table-column v-else
@@ -74,8 +75,9 @@
         </el-table-column>
       </template>
       <el-table-column label="Action" align="center" width="160">
-        <template>
-          <el-button type="warning" icon="el-icon-edit" size="mini">Edit</el-button>
+        <template slot-scope="scope">
+          <el-button type="warning" icon="el-icon-edit" size="mini" @click="onEdit(scope.row)"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="onRemove(scope.row)"></el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -90,27 +92,28 @@ import {
 export default {
   name: 'NodeList',
   data () {
-    let tableData = []
-    for (let i = 0; i < 50; i++) {
-      tableData.push({
-        node_name: `Node ${Math.floor(Math.random() * 100)}`,
-        node_ip: '127.0.0.1:8888',
-        'node_description': `The ID of the node is ${Math.random().toString().replace('0.', '')}`,
-        status: Math.floor(Math.random() * 100) % 2
-      })
-    }
+    // let tableData = []
+    // for (let i = 0; i < 50; i++) {
+    //   tableData.push({
+    //     node_name: `Node ${Math.floor(Math.random() * 100)}`,
+    //     node_ip: '127.0.0.1:8888',
+    //     'node_description': `The ID of the node is ${Math.random().toString().replace('0.', '')}`,
+    //     status: Math.floor(Math.random() * 100) % 2
+    //   })
+    // }
     return {
-      activeForm: {},
+      isEditMode: false,
       dialogVisible: false,
       filter: {
         keyword: ''
       },
-      tableData,
+      // tableData,
       columns: [
-        { name: 'node_name', label: 'Name', width: '160' },
-        { name: 'node_ip', label: 'IP', width: '160' },
+        { name: 'name', label: 'Name', width: '160' },
+        { name: 'ip', label: 'IP', width: '160' },
+        { name: 'port', label: 'Port', width: '80' },
         { name: 'status', label: 'Status', width: '120', sortable: true },
-        { name: 'node_description', label: 'Description', width: 'auto' }
+        { name: 'description', label: 'Description', width: 'auto' }
       ],
       nodeFormRules: {
         name: [{ required: true, message: 'Required Field', trigger: 'change' }]
@@ -119,11 +122,11 @@ export default {
   },
   computed: {
     ...mapState('node', [
-      // 'nodeList',
+      'nodeList',
       'nodeForm'
     ]),
     filteredTableData () {
-      return this.tableData.filter(d => {
+      return this.nodeList.filter(d => {
         if (!this.filter.keyword) return true
         for (let i = 0; i < this.columns.length; i++) {
           const colName = this.columns[i].name
@@ -140,6 +143,7 @@ export default {
       console.log(value)
     },
     onAdd () {
+      this.isEditMode = false
       this.dialogVisible = true
     },
     onAddSubmit () {
@@ -161,7 +165,31 @@ export default {
     onDialogClose () {
       this.$store.commit('node/SET_NODE_FORM', {})
       this.dialogVisible = false
+    },
+    onEdit (row) {
+      console.log(row)
+      this.isEditMode = true
+      this.$store.commit('node/SET_NODE_FORM', row)
+      this.dialogVisible = true
+    },
+    onRemove (row) {
+      this.$confirm('Are you sure to delete this node?', 'Notification', {
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        this.$store.dispatch('node/deleteNode', row._id.$oid)
+          .then(() => {
+            this.$message({
+              type: 'success',
+              message: 'Deleted successfully'
+            })
+          })
+      })
     }
+  },
+  created () {
+    this.$store.dispatch('node/getNodeList')
   }
 }
 </script>
@@ -182,5 +210,9 @@ export default {
   .table {
     margin-top: 20px;
     border-radius: 5px;
+  }
+
+  .delete-confirm {
+    background-color: red;
   }
 </style>
